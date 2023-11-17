ff30b22e2ff2f9dddcf25095ee54693864532d59
Breno Braga <141048792+brenonevs@users.noreply.github.com>
2023-11-16 21:34:34 -0300

    Atualização dos códigos

---
+++ Heatmap/XMLReaderMatriz.py
@@ -0,0 +1,221 @@
+import xml.etree.ElementTree as ET
+import tkinter as tk
+from tkinter import filedialog
+import os
+import regex as re
+
+def LeituraXML(arquivo):
+    tree = ET.parse(arquivo)
+    root = tree.getroot()
+    lista = []
+
+    for elem in root.iter():
+        if elem.tag == 'response' or elem.tag == 'plugin_time':
+            lista.append(elem.attrib)
+
+    return lista
+
+def verifica_intersecao(principal, comparacao):
+    x1_principal, x2_principal = principal[0]
+    y1_principal, y2_principal = principal[1]
+
+    x1_comparacao, x2_comparacao = comparacao[0]
+    y1_comparacao, y2_comparacao = comparacao[1]
+
+    intersecao_x = (x1_principal <= x2_comparacao) and (x1_comparacao <= x2_principal)
+    intersecao_y = (y1_principal <= y2_comparacao) and (y1_comparacao <= y2_principal)
+
+    return intersecao_x and intersecao_y
+
+def processa_matrizes_e_escreve(arquivo, leitura_arquivo_xml, tamanho_matriz):
+    diretorio_script = os.path.dirname(os.path.realpath(__file__))
+
+    caminho_completo_arquivo = os.path.join(diretorio_script, arquivo)
+
+    with open(caminho_completo_arquivo, 'w') as f_saida:
+        matriz_principal = None
+        tempo_inicial_regiao = None
+        ultimo_tempo_regiao = None
+
+        for dados in leitura_arquivo_xml:
+            x_original = int(dados.get('source_file_line', 'N/A'))
+            y_original = int(dados.get('source_file_col', 'N/A'))
+            plugin_time_atual = int(dados.get('plugin_time', 'N/A'))
+
+            matriz_atual = ((x_original - tamanho_matriz // 2, x_original + tamanho_matriz // 2),
+                            (y_original - tamanho_matriz // 2, y_original + tamanho_matriz // 2))
+
+            if matriz_principal is None:
+                matriz_principal = matriz_atual
+                tempo_inicial_regiao = plugin_time_atual
+                ultimo_tempo_regiao = plugin_time_atual
+
+                linha = f'{matriz_atual} {plugin_time_atual}\n'
+                f_saida.write(linha)
+
+            elif verifica_intersecao(matriz_principal, matriz_atual):
+                ultimo_tempo_regiao = plugin_time_atual
+
+                linha = f'{matriz_atual} {plugin_time_atual}\n'
+                f_saida.write(linha)
+
+            else:
+                duracao = ultimo_tempo_regiao - tempo_inicial_regiao
+
+                info_duracao = f'Duracao: {duracao} ms\n\n'
+                f_saida.write(info_duracao)
+
+                matriz_principal = matriz_atual
+                tempo_inicial_regiao = plugin_time_atual
+                ultimo_tempo_regiao = plugin_time_atual
+
+                nova_linha = f'{matriz_atual} {plugin_time_atual}\n'
+                f_saida.write(nova_linha)
+
+        if matriz_principal is not None and ultimo_tempo_regiao is not None:
+            duracao_final = ultimo_tempo_regiao - tempo_inicial_regiao
+            info_duracao_final = f'Duracao: {duracao_final} ms\n'
+            f_saida.write(info_duracao_final)
+
+def escreve_pontos_e_duracao(arquivo, leitura_arquivo_xml, tamanho_matriz):
+    diretorio_script = os.path.dirname(os.path.realpath(__file__))
+
+    nome_arquivo_pontos = f"{os.path.splitext(arquivo)[0]}_pontos.txt"
+    caminho_completo_arquivo_pontos = os.path.join(diretorio_script, nome_arquivo_pontos)
+
+    with open(caminho_completo_arquivo_pontos, 'w') as f_saida_pontos:
+        matriz_principal = None
+        tempo_inicial_regiao = None
+        ultimo_tempo_regiao = None
+        pontos_regiao = []
+
+        for dados in leitura_arquivo_xml:
+            x_original = int(dados.get('source_file_line', 'N/A'))
+            y_original = int(dados.get('source_file_col', 'N/A'))
+            plugin_time_atual = int(dados.get('plugin_time', 'N/A'))
+
+            matriz_atual = ((x_original - tamanho_matriz // 2, x_original + tamanho_matriz // 2),
+                            (y_original - tamanho_matriz // 2, y_original + tamanho_matriz // 2))
+
+            if matriz_principal is None:
+                matriz_principal = matriz_atual
+                tempo_inicial_regiao = plugin_time_atual
+                ultimo_tempo_regiao = plugin_time_atual
+                pontos_regiao.append((x_original, y_original))
+
+            elif verifica_intersecao(matriz_principal, matriz_atual):
+                ultimo_tempo_regiao = plugin_time_atual
+                pontos_regiao.append((x_original, y_original))
+
+            else:
+                duracao = ultimo_tempo_regiao - tempo_inicial_regiao
+                info_duracao = f'Duracao: {duracao} ms\n'
+                f_saida_pontos.write(info_duracao)
+
+                for ponto in pontos_regiao:
+                    f_saida_pontos.write(f'{ponto}\n')
+                f_saida_pontos.write('\n')  
+
+                matriz_principal = matriz_atual
+                tempo_inicial_regiao = plugin_time_atual
+                ultimo_tempo_regiao = plugin_time_atual
+                pontos_regiao = [(x_original, y_original)]
+
+        if matriz_principal is not None and ultimo_tempo_regiao is not None and pontos_regiao:
+            duracao_final = ultimo_tempo_regiao - tempo_inicial_regiao
+            info_duracao_final = f'Duracao: {duracao_final} ms\n'
+            f_saida_pontos.write(info_duracao_final)
+
+            for ponto in pontos_regiao:
+                f_saida_pontos.write(f'{ponto}\n')
+
+    print(f"Dados de pontos armazenados em {caminho_completo_arquivo_pontos}")
+    return caminho_completo_arquivo_pontos  
+
+
+def filtra_duracoes_e_escreve(input_file_path, min_duration):
+
+    output_file_name = 'output_filtered_coordinates.txt'
+    
+    with open(input_file_path, 'r') as file:
+        content = file.read()
+
+    blocks = re.split(r'Duracao: (\d+) ms', content)
+
+    if not blocks[0].strip():
+        blocks = blocks[1:]
+
+    filtered_content = []
+
+
+    for i in range(0, len(blocks), 2):  
+        duration = int(blocks[i].strip()) 
+        coordinates = blocks[i + 1].strip()  
+
+        if duration >= min_duration:
+            filtered_content.append(f'Duracao: {duration} ms\n{coordinates}')
+
+    if filtered_content:
+        with open(output_file_name, 'w') as output_file:
+            output_file.write('\n\n'.join(filtered_content))
+        print(f"Arquivo criado com sucesso: {output_file_name}")
+    else:
+        print("Nenhuma coordenada encontrada com a duração mínima especificada.")
+
+def consolidar_coordenadas(arquivo_entrada):
+
+    nome_base = os.path.basename(arquivo_entrada)
+    nome_sem_extensao = os.path.splitext(nome_base)[0]
+    arquivo_saida = f"{nome_sem_extensao}_coordenadas.txt"
+
+    diretorio_script = os.path.dirname(os.path.realpath(__file__))
+    caminho_completo_arquivo = os.path.join(diretorio_script, arquivo_saida)
+
+    regex_coordenadas = re.compile(r'\((\d+),\s*(\d+)\)')
+
+    try:
+        with open(arquivo_entrada, 'r') as f_entrada, open(caminho_completo_arquivo, 'w') as f_saida:
+            for linha in f_entrada:
+                linha_limpa = linha.strip()
+                if linha_limpa and not linha_limpa.startswith('Duracao:'):
+
+                    match = regex_coordenadas.search(linha_limpa)
+                    if match:
+                        x, y = match.groups()  
+                        f_saida.write(f"{x} {y}\n")  
+
+    except FileNotFoundError:
+        print(f"O arquivo {arquivo_entrada} não foi encontrado.")
+    except Exception as e:
+        print(f"Um erro ocorreu: {e}")
+
+    print(f"Coordenadas consolidadas foram escritas em {caminho_completo_arquivo}")
+
+def selecionar_arquivo():
+    root = tk.Tk()
+    root.withdraw()
+
+    caminho_arquivo = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
+
+    if caminho_arquivo:
+        dados_xml = LeituraXML(caminho_arquivo)
+        nome_base = os.path.basename(caminho_arquivo)
+        nome_sem_extensao = os.path.splitext(nome_base)[0]
+        novo_nome_arquivo = f"{nome_sem_extensao}_processed.txt"
+
+        linha = int(input("Digite quantas linhas tem a matriz: "))
+        coluna = int(input("Digita quantas colunas tem a matriz: "))
+
+        processa_matrizes_e_escreve(novo_nome_arquivo, dados_xml, linha)
+        
+        arquivo_pontos_duracao = escreve_pontos_e_duracao(novo_nome_arquivo, dados_xml, coluna)
+
+        duracao_minima_usuario = int(input("Digite o tempo mínimo de duração (em ms) que deseja analisar: "))
+
+        filtra_duracoes_e_escreve(arquivo_pontos_duracao, duracao_minima_usuario) 
+
+        consolidar_coordenadas("output_filtered_coordinates.txt")
+
+
+if __name__ == "__main__":
+    selecionar_arquivo()
--- Heatmap/heatmap.py
+++ Heatmap/heatmap.py
@@ -2,21 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import gaussian_kde
-from matplotlib.colors import Normalize
-from matplotlib.ticker import PercentFormatter
-import tkinter as tk
-from tkinter import filedialog
+import matplotlib.colors as mcolors
 
 def analisar_e_plotar(caminho_arquivo):
-    
-    with open('Main.java', 'r') as file:
-        content = file.readlines()
-
-    num_lines = len(content)
-    num_columns = max(len(line) for line in content)
-
-    print(num_columns, num_lines)
-
     data = pd.read_csv(caminho_arquivo, header=None, delim_whitespace=True, names=["x", "y"])
 
     x = data.y.values 
@@ -26,7 +14,7 @@ def analisar_e_plotar(caminho_arquivo):
     d = density([x,y])
 
     plt.figure(figsize=(12, 8))
-    ax = sns.kdeplot(x=x, y=y, cmap='Reds', fill=True, cbar=True, cbar_kws={'label': 'Porcentagem Relativa (%)'}) 
+    ax = sns.kdeplot(x=x, y=y, cmap='Reds', fill=True) 
     plt.gca().invert_yaxis()
 
     x_min = x.min() - 20
@@ -47,15 +35,50 @@ def analisar_e_plotar(caminho_arquivo):
     plt.xlabel('Coordenada X (Colunas)')
     plt.ylabel('Coordenada Y (Linhas)')
 
-    plt.show()
+    return plt
+
+def calcular_duracao_max_min(nome_arquivo):
+    with open(nome_arquivo, 'r') as arquivo:
+        linhas = arquivo.readlines()
+
+    duracoes = []
+    for linha in linhas:
+        if linha.startswith("Duracao:"):
+            duracao = int(linha.split(":")[1].strip().split()[0])
+            duracoes.append(duracao)
+
+    if duracoes:
+        duracao_maxima = max(duracoes)
+        duracao_minima = min(duracoes)
+        return duracao_maxima, duracao_minima
+    else:
+        return None, None
+
+def criar_colorbar_gradiente(duracao_maxima, duracao_minima):
+    fig, ax = plt.subplots(figsize=(1, 3)) 
+    
+    cores = mcolors.LinearSegmentedColormap.from_list('gradiente', [(1, 1, 1), (1, 0, 0)], N=256)
+    
+    cbar = plt.colorbar(plt.cm.ScalarMappable(cmap=cores), cax=ax, orientation='vertical',
+                        ticks=[0, 1]) 
+    
+    cbar.ax.set_yticklabels([f'{duracao_minima} ms', f'{duracao_maxima} ms'])
+    cbar.set_label('Duração')
+    
+    plt.subplots_adjust(left=0.452, bottom=0.11, right=0.583, top=0.88, wspace=0.2, hspace=0.2)
 
 def selecionar_arquivo():
-    root = tk.Tk()  
-    root.withdraw()  
 
-    caminho_arquivo = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    caminho_arquivo = "output_filtered_coordinates_coordenadas.txt"
 
     if caminho_arquivo: 
-        analisar_e_plotar(caminho_arquivo)
+        plot_heatmap = analisar_e_plotar(caminho_arquivo)
+        nome_arquivo = 'output_filtered_coordinates.txt'
+        duracao_maxima, duracao_minima = calcular_duracao_max_min(nome_arquivo)
+        if duracao_maxima is not None and duracao_minima is not None:
+            criar_colorbar_gradiente(duracao_maxima, duracao_minima)
+            plt.show() 
+        else:
+            print("Nenhuma duração encontrada no arquivo.")
 
 selecionar_arquivo()
